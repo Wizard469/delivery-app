27db8511e65b4a6b6b9f280f3bfb9bed0f8d6098
Renato Mendes <mendeslrenato@gmail.com>
2023-01-24 19:47:31 -0300

    fix lint errors

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const productsRouter = require('./products-route');
 const usersRouter = require('./users-route');
-const adminRouter = require('./adminRouter')
+const adminRouter = require('./adminRouter');
 
 const routes = Router();
 
 routes.use('/login', usersRouter);
 routes.use('/products', productsRouter);
-app.use('/admin/manage', adminRouter);
+routes.use('/admin/manage', adminRouter);
 
 module.exports = routes;
