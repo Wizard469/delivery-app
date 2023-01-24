cbbc27d84e1ef7028bddd8c9225e9cf55405c62a
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-01-24 15:12:08 -0300

    Adjusting routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Products from './pages/customer/products';
 import Provider from './context/Provider';
-import SellerOrders from './pages/seller/sellerOrders';
+import Routes from './Routes';
 
 function App() {
   return (
     <Provider>
-      <Switch>
-        <Route path="/customer/products" component={ Products } />
-        <Route exact path="/seller/orders" component={ SellerOrders } />
-      </Switch>
+      <Routes />
     </Provider>
   );
 }
