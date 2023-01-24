914f835060c038952d9e8d7e47c2d4ed8370c7e3
marcelamchdo <arqmrcl@gmail.com>
2023-01-24 19:57:12 -0300

    back

--- back-end/src/routes/sales-route.js
+++ back-end/src/routes/sales-route.js
@@ -3,6 +3,10 @@ const { sales } = require('../controller/sales-controller');
 
 const router = Router();
 
+router.post('/', sales.newSale);
+
 router.get('/', sales.sales);
+
+router.get('/:id', salesById);
 
 module.exports = router;
