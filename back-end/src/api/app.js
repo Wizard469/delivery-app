8aef6180fd99beba744c897201b570ff6b0b4752
marcelamchdo <arqmrcl@gmail.com>
2023-01-24 19:57:41 -0300

    back

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const productsRouter = require('../routes/products-route');
 const salesRouter = require('../routes/sales-route');
-// const { default: ErrorHandler } = require('./utils/error/errorMiddleware');
+const { default: ErrorHandler } = require('./utils/error/errorMiddleware');
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
-// app.use(ErrorHandler);
+app.use(ErrorHandler);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
