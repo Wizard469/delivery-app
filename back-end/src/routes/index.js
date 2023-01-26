2dc36d59777084f70b4e352b09e183eb2b373e78
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-01-26 19:36:23 -0300

    Just for fun

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const productsRouter = require('./products-route');
-const usersRouter = require('./users-route');
+const loginRouter = require('./login-route');
+const registerRouter = require('./register-route');
 const adminRouter = require('./adminRouter');
 const salesRouter = require('./sales-route');
 
 const routes = Router();
 
-routes.use('/login', usersRouter);
+routes.use('/login', loginRouter);
+routes.use('/register', registerRouter);
 routes.use('/products', productsRouter);
 routes.use('/admin/manage', adminRouter);
 routes.use('/sales', salesRouter);
--- front-end/src/services/userApi.js
+++ front-end/src/services/userApi.js
@@ -2,9 +2,11 @@ const baseURL = 'http://localhost:3001';
 
 const postRequest = async (endpoint, data) => {
   const response = await fetch(`${baseURL}${endpoint}`, data);
+  console.log(response);
   if (response.ok) {
     const json = await response.json();
 
+    console.log(json);
     return json;
   }
 
