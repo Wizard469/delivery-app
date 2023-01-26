e09a42496402487a9d0d57e417141e64f863bfbd
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-01-26 20:59:19 -0300

    Reqs [9, 10] - Have already passed on the previous commit. Just some adjusts now

--- front-end/src/services/userApi.js
+++ front-end/src/services/userApi.js
@@ -2,11 +2,10 @@ const baseURL = 'http://localhost:3001';
 
 const postRequest = async (endpoint, data) => {
   const response = await fetch(`${baseURL}${endpoint}`, data);
-  console.log(response);
+
   if (response.ok) {
     const json = await response.json();
 
-    console.log(json);
     return json;
   }
 
