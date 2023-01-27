a37a08da33f9373f7c1f20c08153788cf84a8f13
Renato Mendes <mendeslrenato@gmail.com>
2023-01-26 21:46:07 -0300

    fix bug

--- back-end/src/routes/login-route.js
+++ back-end/src/routes/login-route.js
@@ -4,6 +4,7 @@ const usersController = require('../controller/users-controller');
 const router = Router();
 
 router.post('/', usersController.login);
+
 router.get('/sellers', usersController.getSellers);
 
 module.exports = router;
